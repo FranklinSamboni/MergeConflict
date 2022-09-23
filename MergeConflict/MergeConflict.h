b93e308196b18cce3c7c45f80d2bdd7ad151bb79
franklin.samboni <franklin.samboni@globant.com>
2022-09-23 16:56:43 -0500

    Initial Commit

---
+++ MergeConflict/MergeConflict.h
@@ -0,0 +1,18 @@
+//
+//  MergeConflict.h
+//  MergeConflict
+//
+//  Created by Franklin Samboni Castillo on 23/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MergeConflict.
+FOUNDATION_EXPORT double MergeConflictVersionNumber;
+
+//! Project version string for MergeConflict.
+FOUNDATION_EXPORT const unsigned char MergeConflictVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MergeConflict/PublicHeader.h>
+
+
